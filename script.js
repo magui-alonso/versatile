97b2c0c17c1a7dea2cb14d92a0f8133637f058bd
magui-alonso <magui.alonso.g@gmail.com>
2021-01-28 20:25:31 -0300

    cambios en js

--- script.js
+++ script.js
@@ -1,86 +1,65 @@
-//Clase 6
+//Entrega proyecto final
 
-function ProductosEnStock(modelo, talle, color) {
-    this.modelo = modelo;
-    this.talle = talle;
-    this.color = color;
-    
-}
-
-let carrito = [
-    new ProductosEnStock("Biker Lycra", "S/M/L", "Negra"),
-    new ProductosEnStock("Biker Lycra", "S/M/L", "Rosa"),
-    new ProductosEnStock("Biker Lycra", "S/M/L", "Verde"),
-    new ProductosEnStock("Biker Saten", "S/M/L", "Negro"),
-    new ProductosEnStock("Biker Saten", "S/M/L", "Suela"),
-    new ProductosEnStock("Biker Saten", "S/M/L", "Blanca"),
-    new ProductosEnStock("Biker Tornasolada", "S/M/L", "Azul"),
-    new ProductosEnStock("Biker Tornasolada", "S/M/L", "Champagne"),
-    
-
-]
-
-console.log (carrito);
-for(let i=0; i<carrito.length; i++){
-    console.log (carrito[i]); 
-}
-
-
-const Productos = [{
-        id: "1"
+const productosVersatile = [{
+        id: 1,
         nombre: "Biker Lycra",
         precio: 1000
     },
     {
-        id: "2"
+        id: 2,
         nombre: "Biker Lycra",
         precio: 1000
     },
     {
-        id: "3"
+        id: 3,
         nombre: "Biker Lycra",
         precio: 1000
     },
     {
-        id: "4"
+        id: 4,
         nombre: "Biker Saten",
         precio: 1100
     },
     {
-        id: "5"
+        id: 5,
         nombre: "Biker Saten",
         precio: 1100
     },
     {
-        id: "6"
+        id: 6,
         nombre: "Biker Saten",
         precio: 1100
     },
     {
-        id: "7"
+        id: 7,
         nombre: "Biker Tornasolada",
         precio: 1300
     },
     {
-        id: "8"
+        id: 8,
         nombre: "Biker Tornasolada",
         precio: 1300
     },
 
 ]
-Productos.forEach((el) => {
-    console.log(el.nombre)
+
+productosVersatile.forEach((producto) => {
+    console.log(producto)
 })
 
+let carrito = []
 
-console.log(Productos.map((el) => el.precio))
+function agregarAlCarrito(id) {
 
-function sumar(array) {
-    sumaTotal = 0
-    for (let el of array) {
-        sumaTotal += el.precio;
-    }
-    return sumaTotal
+    let productoElegido = productosVersatile.filter((el) => el.id == id)
+    carrito.push(productoElegido[0])
 }
 
-console.log(Productos.reduce((acc, el) => acc + el.precio, 0))
+agregarAlCarrito(5)
+agregarAlCarrito(3)
+agregarAlCarrito(2)
+
+console.log(carrito)
+
+console.log(carrito.reduce((acc, el) => acc + el.precio, 0))
+
