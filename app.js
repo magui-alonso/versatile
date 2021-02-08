e9fa81a82c31ff36ca030a90fbdfa14d2940ffe9
magui-alonso <magui.alonso.g@gmail.com>
2021-02-08 20:07:21 -0300

    carrito funcionando

--- app.js
+++ app.js
@@ -1,22 +1,81 @@
-
-
-
 let carritoDeCompras = []
 
 const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
 
-stockProductos.forEach((productoNuevo) => {
-    let div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML += `
-                    <img src=${productoNuevo.img} alt="">
-                    <h5>${productoNuevo.nombre}</h5>
-                    <p>${productoNuevo.color}</p>
-                    <p>Talle: ${productoNuevo.talle}</p>
-                    <p class="precioProducto">Precio: $${productoNuevo.precio}</p>
-                    <button id="boton${productoNuevo.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
-        `
-    contenedorProductos.appendChild(div)
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
+const selecModelo = document.getElementById('selecModelo')
+selecModelo.addEventListener('change', ()=>{
+    console.log(selecModelo.value)
     
 
+    if (selecModelo.value == "all") {
+        mostrarProductos(stockProductos)
+    } else {
+        mostrarProductos(stockProductos.filter((el)=> el.modelo == selecModelo.value))
+    }
+
 })
+
+
+mostrarProductos(stockProductos)
+
+function mostrarProductos(array) {
+    contenedorProductos.innerHTML = ''
+    array.forEach((productoNuevo) => {
+        let div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML += `
+                    <div class="contenedor-img">
+                        <img class="card-img-top" src=${productoNuevo.img} alt="imagen-producto">
+                    </div> 
+                    <h5 class="card-title">${productoNuevo.modelo}</h5>
+                    <p class="card-text">Color ${productoNuevo.color}</p>
+                    <p class="card-text">Talle: ${productoNuevo.talle}</p>
+                    <p class="precioProducto">Precio: $${productoNuevo.precio}</p>
+                    <button id="boton${productoNuevo.id}" class="boton-agregar"><i class="fas fa-cart-plus"></i></button>
+        `
+        contenedorProductos.appendChild(div)
+        let boton = document.getElementById(`boton${productoNuevo.id}`)
+
+
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(productoNuevo.id)
+        })
+    })
+}
+
+function agregarAlCarrito(id) {
+
+    let productoAgregar = stockProductos.filter((el) => el.id == id)[0]
+    carritoDeCompras.push(productoAgregar)
+    console.log(carritoDeCompras)
+    actualizarCarrito()
+
+    let div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+    div.innerHTML = `
+        <img class="imagenProdCarrito" src=${productoAgregar.img} alt="imagen-producto">
+        <div class="textoProdCarrito">
+            <p>${productoAgregar.modelo}</p>
+            <p>Color ${productoAgregar.color}</p>
+        </div>
+        <p>Precio: $${productoAgregar.precio}</p>
+        <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+    `
+    contenedorCarrito.appendChild(div)
+    let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+
+    botonEliminar.addEventListener('click', () => {
+        botonEliminar.parentElement.remove()
+        carritoDeCompras = carritoDeCompras.filter((el) => el.id != productoAgregar.id)
+        actualizarCarrito()
+    })
+}
+
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.length
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+}
--- stock.js
+++ stock.js
@@ -1,12 +1,12 @@
 let stockProductos = [
-    {id: 1, nombre: "Byker Lycra", color: "Negra", precio: 1200, talle: "S", img: "multimedia/productos/biker-lycra.jpeg"},
-    {id: 2, nombre: "Byker Lycra", color: "Azul", precio: 1200, talle: "M", img: "multimedia/productos/biker-lycra.jpeg"},
-    {id: 3, nombre: "Byker Lycra", color: "Rosa", precio: 1200, talle: "L", img: "multimedia/productos/biker-lycra.jpeg"},
-    {id: 4, nombre: "Byker Saten", color: "Negra", precio: 1300, talle: "S", img: "multimedia/productos/biker-saten.jpeg"},
-    {id: 5, nombre: "Byker Saten", color: "Camel", precio: 1300, talle: "M", img: "multimedia/productos/biker-saten.jpeg"},
-    {id: 6, nombre: "Byker Saten", color: "Verde", precio: 1300, talle: "L", img: "multimedia/productos/biker-saten.jpeg"},
-    {id: 7, nombre: "Byker Tornasolada", color: "Azul", precio: 1500, talle: "S", img: "multimedia/productos/biker-tornasolada.jpeg"},
-    {id: 8, nombre: "Byker Tornasolada", color: "Champagne", precio: 1500, talle: "M", img: "multimedia/productos/biker-tornasolada.jpeg"},
-    {id: 9, nombre: "Byker Tornasolada", color: "Negra", precio: 1500, talle: "L", img: "multimedia/productos/biker-tornasolada.jpeg"},
+    {id: 1, modelo: "Byker Lycra", color: "Negra", precio: 1200, talle: "S", img: "multimedia/productos/biker-lycra.jpeg"},
+    {id: 2, modelo: "Byker Lycra", color: "Azul", precio: 1200, talle: "M", img: "multimedia/productos/biker-lycra.jpeg"},
+    {id: 3, modelo: "Byker Lycra", color: "Rosa", precio: 1200, talle: "L", img: "multimedia/productos/biker-lycra.jpeg"},
+    {id: 4, modelo: "Byker Saten", color: "Negra", precio: 1300, talle: "S", img: "multimedia/productos/biker-saten.jpg"},
+    {id: 5, modelo: "Byker Saten", color: "Camel", precio: 1300, talle: "M", img: "multimedia/productos/biker-saten.jpg"},
+    {id: 6, modelo: "Byker Saten", color: "Verde", precio: 1300, talle: "L", img: "multimedia/productos/biker-saten.jpg"},
+    {id: 7, modelo: "Byker Tornasolada", color: "Azul", precio: 1500, talle: "S", img: "multimedia/productos/biker-tornasolada.jpeg"},
+    {id: 8, modelo: "Byker Tornasolada", color: "Champagne", precio: 1500, talle: "M", img: "multimedia/productos/biker-tornasolada.jpeg"},
+    {id: 9, modelo: "Byker Tornasolada", color: "Negra", precio: 1500, talle: "L", img: "multimedia/productos/biker-tornasolada.jpeg"},
     
 ]
